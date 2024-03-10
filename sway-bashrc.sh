6e806f43a151c8ad0830d761d90689234ea2e43d
RadioNoiseE <j18516785606@icloud.com>
2024-03-10 22:29:41 +0800

    small sync

--- sway-bashrc.sh
+++ sway-bashrc.sh
@@ -1,14 +1,27 @@
 # /etc/skel/.bashrc
 
-export WLR_RENDERER=vulkan
-export GTK_IM_MODULE=fcitx # emacs GDK_IS_WAYLAND_DISPLAY assertion fail
+export WLR_RENDERER=gles2 # swaybg WL_DISPLAY_ROUNDTRIP failed for vulkan
+export GTK_IM_MODULE=fcitx # emacs GDK_IS_WAYLAND_DISPLAY assertion fail for wayland
 export QT_IM_MODULE=fcitx
 export XMODIFIERS=@im=fcitx
+export MOZ_ENABLE_WAYLAND=1 # firefox force wayland
 
 if [ -z "${WAYLAND_DISPLAY}" ] && [ ${XDG_VTNR} -eq 1 ]; then
     clear
-    uname -a
-    dbus-run-session sway
+  # uname -a
+    dbus-run-session sway &>/dev/null
 fi
 
-complete -cf doas
+# complete -cf doas
+
+PATH=/usr/local/texlive/2023/bin/x86_64-linux:$PATH; export PATH
+MANPATH=/usr/local/texlive/2023/texmf-dist/doc/man:$MANPATH; export MANPATH
+INFOPATH=/usr/local/texlive/2023/texmf-dist/doc/info:$INFOPATH; export INFOPATH
+
+alias proxy="doas anyconnect ~/.anyconnect"
+
+# opam configuration
+test -r /home/rne/.opam/opam-init/init.sh && . /home/rne/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true
+
+# set fish as default user shell
+[ -x /bin/fish ] && SHELL=/bin/fish exec fish
